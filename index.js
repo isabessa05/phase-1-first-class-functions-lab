9a1eb92fa3cf4f570ffc20bb9688f6c6d4d7cf75
+ <+>
2022-08-09 13:10:37 -0400

    commit tree 1872872652e593d3230fcd44e9fce7dccd6df403

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solution in this file!
+function returnFirstTwoDrivers(array){
+    let newArray = [];
+    newArray.push (array[0]);
+    newArray.push (array[1]);
+    return newArray
+}
+
+function returnLastTwoDrivers (array){
+    let newArray2 = [];
+    newArray2.push(array[array.length - 2]);
+    newArray2.push(array[array.length - 1]);
+    
+    return newArray2
+};
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier (fare){
+
+    return  function fareMultiplier(multiplier){
+        return fare * multiplier;
+    }
+}
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+const selectDifferentDrivers = function (drivers, driversToReturn){
+    return driversToReturn (drivers)
+}
+
+
+
